8fd6e6ef4658c825171ffb5317a3e85f1ada9d8c
Alexander Todorov <41835213+tdrv90@users.noreply.github.com>
2020-06-07 17:35:44 +0300

    Adding "Create Accesory" functionality.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const { getAllCubes, getCube } = require('../controllers/cubes');
 const Cube = require('../models/cube');
+const Accessory = require('../models/accessory');
 
 module.exports = (app) => {
     app.get('/', async (req, res) => {
@@ -46,6 +47,36 @@ module.exports = (app) => {
             ...cube
         });
     })
+
+    app.get('/create/accessory', (req, res) => {
+        res.render('createAccessory', {
+            title: 'Create accessory'
+        })
+    })
+
+    app.post('/create/accessory', async (req, res) => {
+        const {
+            name,
+            description,
+            imageUrl
+        } = req.body;
+
+        const accessory = new Accessory({ name, description, imageUrl });
+
+        await accessory.save();
+
+        res.redirect('/create/accessory');
+    })
+
+    app.get('/attach/accessory/:id', async (req, res) => {
+        const cube = await getCube(req.params.id)
+
+        res.render('attachAccessory', {
+            title: 'Attach accessory',
+            ...cube
+        })
+    })
+
 
     app.get('*', (req, res) => {
         res.render('404', {
