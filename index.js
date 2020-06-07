21b76f18faa197ed5327c36ae5c664f5d32788e2
Alexander Todorov <41835213+tdrv90@users.noreply.github.com>
2020-06-07 16:27:42 +0300

    connect with database

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const env = process.env.NODE_ENV || 'development';
+const mongoose = require('mongoose')
 
 const config = require('./config/config')[env];
 const express = require('express');
 const app = express();
+
+mongoose.connect(config.databaseUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if (err) return console.error(err);
+
+    console.log('Database is setup and running.');
+})
 
 require('./config/express')(app);
 require('./routes')(app);
