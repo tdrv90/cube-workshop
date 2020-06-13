54b7f2ec9d40a2aa256a9fb8cc47764edce69e07
Alexander Todorov <41835213+tdrv90@users.noreply.github.com>
2020-06-13 15:54:55 +0300

    finish signup

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,8 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
 
+const privateKey = 'CUBE-WORKSHOP'
+
 const saveUser = async (req, res) => {
     const {
         username,
@@ -11,14 +13,22 @@ const saveUser = async (req, res) => {
     // hashing
     const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(password, salt)
-    
+
     const user = new User({
         username,
         password: hashedPassword
     })
 
-    await user.save()
+    const userObject = await user.save()
 
+    const token = jwt.sign({
+        userId: userObject._id,
+        username: userObject.username
+    }, privateKey)
+
+
+    res.cookie('autoid', token)
+    console.log(token)
     return true
 }
 
